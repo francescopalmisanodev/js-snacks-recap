3e6e92c8639371977b538f5a09a76543258ba22a
francescopalmisanodev <francescopalmisano.dev@gmail.com>
2021-10-28 17:08:49 +0200

    v1.1

---
+++ snack-pm/main.js
@@ -0,0 +1,18 @@
+const btn=document.getElementById("btn");
+const calcolo=document.getElementById("calc");
+
+btn.addEventListener("click",function(){
+    const numero=[];
+    let somma=0;
+    let media=0;
+    for (let i = 0; i < 4; i++) {
+        do{
+            numero[i] = parseInt(prompt(`inserisci la ${i+1} cifra del numero:`));
+        }while (isNaN(numero[i]))
+        somma+=numero[i]; 
+    }
+    media=somma/numero.length;
+    console.log(media);
+    console.log(somma);   
+    calcolo.innerHTML=(`<div>La somma è:${somma}, La media è:${media}</div>`);
+})
